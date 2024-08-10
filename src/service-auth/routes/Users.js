f5377e834c95ea4fe87f1f7793b334788a8ddef9
Heros Perez <119890520+hersdev199@users.noreply.github.com>
2024-08-10 22:45:21 +0000

    route login

--- src/service-auth/routes/Users.js
+++ src/service-auth/routes/Users.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { signup, login } = require("../controllers/Users.js");
+const { signup, login, logout } = require("../controllers/Users.js");
 
 //*-*-**-*-*-*-*-*-*-*-*-**-*-*-*-*-*-*-*-*-**-*-*-*-*-*-*-
 //CREATE NEW USER
 router.post("/signup", signup);
 //LOGIN
 router.post("/login", login);
+
+router.get("/logout", logout);
 //*-*-**-*-*-*-*-*-*-*-*-**-*-*-*-*-*-*-*-*-**-*-*-*-*-*-*-
 
 module.exports = router;
